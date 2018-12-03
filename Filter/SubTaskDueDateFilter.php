07d7c20dc7a73934de9499568a89cc0e033e4720
Craig Crosby <creecros@gmail.com>
2018-12-03 14:30:49 -0500

    Update SubTaskDueDateFilter.php

--- Filter/SubTaskDueDateFilter.php
+++ Filter/SubTaskDueDateFilter.php
@@ -5,7 +5,9 @@ namespace Kanboard\Plugin\Subtaskdate\Filter;
 use Kanboard\Core\Filter\FilterInterface;
 use Kanboard\Filter\BaseDateFilter;
 use Kanboard\Model\SubtaskModel;
+use Kanboard\Model\TaskModel;
 use PicoDb\Table;
+use PicoDb\Database;
 
 /**
  * Filter subtasks by due date
@@ -16,6 +18,14 @@ use PicoDb\Table;
  */
 class SubTaskDueDateFilter extends BaseDateFilter implements FilterInterface
 {
+const TABLE = 'subtasks';
+    /**
+     * Database object
+     *
+     * @access private
+     * @var Database
+     */
+    private $db;
     /**
      * Get search attribute
      *
@@ -24,7 +34,19 @@ class SubTaskDueDateFilter extends BaseDateFilter implements FilterInterface
      */
     public function getAttributes()
     {
-        return array('subtask_date');
+        return array('subtask_due');
+    }
+    /**
+     * Set database object
+     *
+     * @access public
+     * @param  Database $db
+     * @return $this
+     */
+    public function setDatabase(Database $db)
+    {
+        $this->db = $db;
+        return $this;
     }
 
     /**
@@ -34,8 +56,35 @@ class SubTaskDueDateFilter extends BaseDateFilter implements FilterInterface
      * @return FilterInterface
      */
     public function apply()
-    {
-        $this->applyDateFilter(SubtaskModel::TABLE . '.due_date');
-        return $this;
+    {        
+        if ($this->value == "none") {
+            $duedate = $this->db
+            ->table(self::TABLE)
+            ->eq('due_date', 0)
+            ->findAllByColumn('task_id');
+             return $this->query->in(TaskModel::TABLE.'.id', $duedate);
+        } else {
+            
+            $method = $this->parseOperator();
+            $timestamp = $this->dateParser->getTimestampFromIsoFormat($this->value);
+            
+            if ($method !== '') {
+                $duedate = $this->db
+                ->table(self::TABLE)
+                ->neq('due_date', 0)
+                ->notNull('due_date')
+                ->$method('due_date', $this->getTimestampFromOperator($method, $timestamp))
+                ->findAllByColumn('task_id');
+            } else {
+                $duedate = $this->db
+                ->table(self::TABLE)
+                ->neq('due_date', 0)
+                ->notNull('due_date')
+                ->gte('due_date', $timestamp)
+                ->lte('due_date', $timestamp + 86399)
+                ->findAllByColumn('task_id');
+            }
+             return $this->query->in(TaskModel::TABLE.'.id', $duedate);
+        }
     }
 }
